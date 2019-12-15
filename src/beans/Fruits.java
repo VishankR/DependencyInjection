f4675c7e37802abd31e61e2552db6ea6095881e1
VishankR <vishankrajput2001@gmail.com>
2019-12-15 17:03:28 +0530

    now for array injection

---
+++ src/beans/Fruits.java
@@ -0,0 +1,15 @@
+package beans;
+
+public class Fruits {
+	private String[] names;
+
+	public String[] getNames() {
+		return names;
+	}
+
+	public void setNames(String[] names) {
+		this.names = names;
+	}
+	
+
+}
